3aa7630843c40703f2f9faff42d31910c4a03c29
justinchan <jktc51@student.cis.edu.hk>
2020-06-17 09:24:39 +0800

    ustin

---
+++ core/src/com/mygdx/game/Constants.java
@@ -0,0 +1,6 @@
+package com.mygdx.game;
+
+public class Constants
+{
+    public static final String LEVEL_MAP_FILENAME = "levelOne.tmx";
+}
--- core/src/com/mygdx/game/Screens/LevelOne.java
+++ core/src/com/mygdx/game/Screens/LevelOne.java
@@ -1,24 +1,43 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Constants;
 
 public class LevelOne implements Screen {
     MyGdxGame myGame;
+    Texture myImg;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     public LevelOne(MyGdxGame game) {
         this.myGame = game;
 
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load(Constants.LEVEL_MAP_FILENAME);
+        renderer = new OrthogonalTiledMapRenderer(map);
     }
     @Override
-    public void show() {
+    public void show()
+    {
+
 
     }
 
     @Override
     public void render(float delta) {
 
-        System.out.println("no u");
+         myGame.getBatch().begin();
+
+         myGame.getBatch().draw(myImg, 0, 0);
+
+         myGame.getBatch().end();
     }
 
     @Override
