f432edafc7f16f8388f6a5c169457fa9b09e5578
justinchan <jktc51@student.cis.edu.hk>
2020-06-17 10:12:15 +0800

    ustin

--- core/src/com/mygdx/game/Constants.java
+++ core/src/com/mygdx/game/Constants.java
@@ -4,6 +4,7 @@ public class Constants
 {
     public static final float WIDTH = 20 * 16;
     public static final float HEIGHT = 16 * 16;
-
     public static final String LEVEL_MAP_FILENAME = "levelOne.tmx";
+
+    public static final int MUSHROOMO_LAYER = 5;
 }
