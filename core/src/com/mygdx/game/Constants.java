568714fb33db0f767289673a321f1005bf194931
justinchan <jktc51@student.cis.edu.hk>
2020-06-17 10:08:06 +0800

    initial commit

--- core/src/com/mygdx/game/Constants.java
+++ core/src/com/mygdx/game/Constants.java
@@ -2,5 +2,8 @@ package com.mygdx.game;
 
 public class Constants
 {
+    public static final float WIDTH = 20 * 16;
+    public static final float HEIGHT = 16 * 16;
+
     public static final String LEVEL_MAP_FILENAME = "levelOne.tmx";
 }
--- core/src/com/mygdx/game/Screens/LevelOne.java
+++ core/src/com/mygdx/game/Screens/LevelOne.java
@@ -1,14 +1,31 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Constants;
 
-public class LevelOne implements Screen {
+public class LevelOne implements Screen
+{
+    MyGdxGame demoGame;
+
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+
+
     MyGdxGame myGame;
     Texture myImg;
 
@@ -16,12 +33,34 @@ public class LevelOne implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
+    private OrthographicCamera camera;
+    private Viewport viewport;
+
+
     public LevelOne(MyGdxGame game) {
         this.myGame = game;
 
         mapLoader = new TmxMapLoader();
         map = mapLoader.load(Constants.LEVEL_MAP_FILENAME);
         renderer = new OrthogonalTiledMapRenderer(map);
+        this.demoGame = game;
+
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load(Constants.LEVEL_MAP_FILENAME);
+        renderer = new OrthogonalTiledMapRenderer(map);
+        this.demoGame = game;
+        world = new World(new Vector2( 0,0), true);
+
+        BodyDef bodyDef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fixtureDef = new FixtureDef();
+        Body body;
+
+        for (: map.getLayers().get(Constants.))
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(Constants.WIDTH, Constants.HEIGHT, camera);
     }
     @Override
     public void show()
@@ -61,7 +100,11 @@ public class LevelOne implements Screen {
     }
 
     @Override
-    public void dispose() {
+    public void dispose()
+    {
+        map.dispose();
+        renderer.dispose();
+
 
     }
 }
